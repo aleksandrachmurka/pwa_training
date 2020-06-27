c0739f827a7f5d20b513586deb2743a917158fa2
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2020-06-27 12:53:08 +0200

    Add offline handling

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,18 +13,39 @@ self.addEventListener('install', (evt) => {
     .then(cache => {
       console.log('[ServiceWorker] Pre caching offline page');
       return cache.addAll(FILES_TO_CACHE);
-    }));
-    
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (evt) => {
   console.log('[ServiceWorker] Activate');
-
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServieWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }))
+    })
+  )
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (evt) => {
   console.log('[ServiceWorker] Fetch', evt.request.url);
+  if (evt.request.mode !== 'navigate') {
+    return;
+  }
+
+  evt.respondWith(
+    fetch(evt.request).catch(()=>{
+      return caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.match('offline.html');
+      })
+    })
+  );
 
 });
